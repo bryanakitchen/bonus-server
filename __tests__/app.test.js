c0497b139447f1f5d0be7788bceb0d642f264560
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-14 13:54:46 -0500

    add post test - red

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,5 +8,27 @@ describe('bonus-server routes', () => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
 
+  it('Adds an animal via POST', async() => {
+    const animal = await animal.insert({
+      name: 'Lion',
+      type: 'mammal',
+      characteristic: 'big mane'
+    });
 
+    return request(app)
+      .post('/api/v1/animals')
+      .send({
+        name: 'Lion',
+        type: 'mammal',
+        characteristic: 'big mane'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          name: 'Lion',
+          type: 'mammal',
+          characteristic: 'big mane'
+        });
+      });
+  });
 });
